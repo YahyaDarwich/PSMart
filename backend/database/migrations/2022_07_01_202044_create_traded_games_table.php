35553dd25911eae6becf6f3efea7001723243590
Yahya <ydarwich18@gmail.com>
2022-07-01 23:24:46 +0300

    create model and migration for tradded games, users table

---
+++ backend/database/migrations/2022_07_01_202044_create_traded_games_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('traded_games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('publisher')->nullable();
+            $table->string('image')->nullable();
+            $table->string('description')->nullable();
+            $table->string('trade_to')->nullable();
+            $table->boolean('approved')->nullable();
+            $table->boolean('rejected')->nullable();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('traded_games');
+    }
+};
