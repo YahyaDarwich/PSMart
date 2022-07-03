734bc409b90f08996cabbfd1fa7f5d9f6a25a247
Yahya <ydarwich18@gmail.com>
2022-07-03 15:14:47 +0300

    secure api with token

--- backend/app/Http/Middleware/JwtMiddleware.php
+++ backend/app/Http/Middleware/JwtMiddleware.php
@@ -9,6 +9,14 @@ use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
 class JwtMiddleware extends BaseMiddleware
 {
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
         try {
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,55 +19,62 @@ use App\Http\Controllers\UserController;
 |
 */
 
+// register
 Route::post('register', [UserController::class, 'register']);
+// login
 Route::post('login', [UserController::class, 'authenticate']);
 
-// Users Routes
-Route::group(['prefix' => 'user'], function () {
-    Route::get('/{id}', [UserController::class, 'get']);
-    Route::put('/{id}', [UserController::class, 'update']);
-    Route::delete('/{id}', [UserController::class, 'delete']);
-});
-Route::get('users', [UserController::class, 'getAllUsers']);
-Route::get('admins', [UserController::class, 'getAllAdmins']);
-Route::get('logeduser', [UserController::class, 'getAuthenticatedUser']);
+Route::group(['middleware' => ['jwt.verify']], function () {
 
-// Genre Routes
-Route::group(['prefix' => 'genre'], function () {
-    Route::get('/', [GenreController::class, 'getAll']);
-    Route::get('/{id}', [GenreController::class, 'get']);
-    Route::post('/', [GenreController::class, 'create']);
-    Route::put('/{id}', [GenreController::class, 'update']);
-    Route::delete('/{id}', [GenreController::class, 'delete']);
+    // Users Routes
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/{id}', [UserController::class, 'get']);
+        Route::put('/{id}', [UserController::class, 'update']);
+        Route::delete('/{id}', [UserController::class, 'delete']);
+    });
+    Route::get('users', [UserController::class, 'getAllUsers']);
+    Route::get('admins', [UserController::class, 'getAllAdmins']);
+    // Get the authenticated user data need authorization token
+    Route::get('logeduser', [UserController::class, 'getAuthenticatedUser']);
+
+    // Genre Routes
+    Route::group(['prefix' => 'genre'], function () {
+        Route::get('/', [GenreController::class, 'getAll']);
+        Route::get('/{id}', [GenreController::class, 'get']);
+        Route::post('/', [GenreController::class, 'create']);
+        Route::put('/{id}', [GenreController::class, 'update']);
+        Route::delete('/{id}', [GenreController::class, 'delete']);
+    });
+
+    // Platform Routes
+    Route::group(['prefix' => 'platform'], function () {
+        Route::get('/', [PlatformController::class, 'getAll']);
+        Route::get('/{id}', [PlatformController::class, 'get']);
+        Route::post('/', [PlatformController::class, 'create']);
+        Route::put('/{id}', [PlatformController::class, 'update']);
+        Route::delete('/{id}', [PlatformController::class, 'delete']);
+    });
+
+    // Game Routes
+    Route::group(['prefix' => 'game'], function () {
+        Route::get('/', [GameController::class, 'getAll']);
+        Route::get('/{id}', [GameController::class, 'get']);
+        Route::post('/', [GameController::class, 'create']);
+        Route::put('/{id}', [GameController::class, 'update']);
+        Route::delete('/{id}', [GameController::class, 'delete']);
+    });
+
+    // Tradded game Routes
+    Route::group(['prefix' => 'traddedgame'], function () {
+        Route::get('/', [TraddedGameController::class, 'getAll']);
+        Route::get('/{id}', [TraddedGameController::class, 'get']);
+        Route::get('/user/{id}', [TraddedGameController::class, 'getByUser']);
+        Route::post('/', [TraddedGameController::class, 'create']);
+        Route::put('/{id}', [TraddedGameController::class, 'update']);
+        Route::delete('/{id}', [TraddedGameController::class, 'delete']);
+    });
 });
 
-// Platform Routes
-Route::group(['prefix' => 'platform'], function () {
-    Route::get('/', [PlatformController::class, 'getAll']);
-    Route::get('/{id}', [PlatformController::class, 'get']);
-    Route::post('/', [PlatformController::class, 'create']);
-    Route::put('/{id}', [PlatformController::class, 'update']);
-    Route::delete('/{id}', [PlatformController::class, 'delete']);
-});
-
-// Game Routes
-Route::group(['prefix' => 'game'], function () {
-    Route::get('/', [GameController::class, 'getAll']);
-    Route::get('/{id}', [GameController::class, 'get']);
-    Route::post('/', [GameController::class, 'create']);
-    Route::put('/{id}', [GameController::class, 'update']);
-    Route::delete('/{id}', [GameController::class, 'delete']);
-});
-
-// Tradded game Routes
-Route::group(['prefix' => 'traddedgame'], function () {
-    Route::get('/', [TraddedGameController::class, 'getAll']);
-    Route::get('/{id}', [TraddedGameController::class, 'get']);
-    Route::get('/user/{id}', [TraddedGameController::class, 'getByUser']);
-    Route::post('/', [TraddedGameController::class, 'create']);
-    Route::put('/{id}', [TraddedGameController::class, 'update']);
-    Route::delete('/{id}', [TraddedGameController::class, 'delete']);
-});
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
