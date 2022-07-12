7f1b5bf843137d97abd05dff564c550b888b2f88
Yahya <ydarwich18@gmail.com>
2022-07-12 15:44:36 +0300

    add one game container

---
+++ frontend/src/components/GameContainer/GameContainer.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./GameContainer.css";
+
+const GameContainer = (props) => {
+  return (
+    <>
+      <div className="one_game_container">
+        <a href="#game">
+          <div className="img_container">
+            <img src={props.img} alt={props.name}></img>
+          </div>
+          <div className="platforms">
+            
+            <span className="platform">{props.platform}</span>
+          </div>
+          <h3 className="name">{props.name}</h3>
+          <span className="price">${props.price}</span>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default GameContainer;
