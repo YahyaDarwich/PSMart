7854697907beeeee41882f067e8787300fa0daae
Yahya <ydarwich18@gmail.com>
2022-07-17 00:08:35 +0300

    design access page to be responsive

--- frontend/src/pages/AccessPage/AccessPage.js
+++ frontend/src/pages/AccessPage/AccessPage.js
@@ -49,14 +49,14 @@ const AccessPage = () => {
           <div class="overlay-container" id="overlayCon">
             <div class="overlay">
               <div class="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
+                <h1>Welcome Back!👋</h1>
                 <p>
                   To keep connected with us please login with your personal info
                 </p>
                 <button>Sign In</button>
               </div>
               <div class="overlay-panel overlay-right">
-                <h1>Hello, Friend!</h1>
+                <h1>Hello, Friend!<br/>🖐</h1>
                 <p>Enter your personal details and start journey with us</p>
                 <button>Sign Up</button>
               </div>
