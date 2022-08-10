8851e41132d2740d0b40058649bd30086a5cf37a
Yahya <ydarwich18@gmail.com>
2022-08-10 23:41:07 +0300

    secure routes with token

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,7 +24,7 @@ Route::post('register', [UserController::class, 'register']);
 // login
 Route::post('login', [UserController::class, 'authenticate']);
 
-//Route::group(['middleware' => ['jwt.verify']], function () {
+Route::group(['middleware' => ['jwt.verify']], function () {
 
     // Users Routes
     Route::group(['prefix' => 'user'], function () {
@@ -74,7 +74,7 @@ Route::post('login', [UserController::class, 'authenticate']);
         Route::post('/{id}', [TraddedGameController::class, 'update']);
         Route::delete('/{id}', [TraddedGameController::class, 'delete']);
     });
-//});
+});
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
